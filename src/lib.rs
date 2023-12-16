fbfc724e3c9227650f97889c46f3a21e9f33c286
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-12-16 22:56:29 +0100

    feat: quick&easy flags with get_flag

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,11 @@
 pub mod generate;
 pub mod graph;
 pub mod node;
+
+thread_local! {
+    pub static FLAGS: std::cell::RefCell<Vec<String>> = std::cell::RefCell::new(Vec::new());
+}
+
+pub fn get_flag(name: &str) -> bool {
+    FLAGS.with(|f| f.borrow().iter().any(|flag| flag == name))
+}
