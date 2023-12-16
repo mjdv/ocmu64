76f06038e776375658116b6a77bb9e9f89eb1350
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-12-16 02:44:03 +0100

    generate --seed

--- src/bin/generate.rs
+++ src/bin/generate.rs
@@ -12,11 +12,13 @@ struct Args {
     /// Optional path to output file, or stdout by default.
     #[clap(short, long)]
     output: Option<PathBuf>,
+    #[clap(short, long)]
+    seed: Option<u64>,
 }
 
 fn main() {
     let args = Args::parse();
-    let g = args.graph_type.generate(None);
+    let g = args.graph_type.generate(args.seed);
     match args.output {
         None => g.to_stdout(),
         Some(f) => g.to_file(&f),
